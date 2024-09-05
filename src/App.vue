94cb5f391cd9d420ed206c8de522c018c76bc114
notayogilee <notayogilee@gmail.com>
2024-09-04 20:31:12 -0700

    Set up state to keep all modifications of gif. Start to add text section.

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,55 @@
 import TheHeader from './components/TheHeader.vue'
 import VideoItem from './components/VideoItem.vue'
 import TheButton from './components/TheButton.vue'
-import { ref, inject } from 'vue'
+import { ref, inject, computed } from 'vue'
 
 //state
-const savedRenderingContexts = ref(null)
-const finishedRecording = ref(false)
+const steps = {
+  recording: {
+    title: 'Start by recording a short video',
+    btnText: 'Start Recording',
+    btnBgColorClass: 'bg-emerald-500',
+    btnTextColorClass: 'text-white'
+  },
+  edit: {
+    title: 'Would you like to customize your gif?',
+    btnText: 'Edit GIF',
+    btnBgColorClass: 'bg-emerald-500',
+    btnTextColorClass: 'text-white'
+  }
+}
+
+const savedRenderingContexts = ref([])
+const currentSavedRenderingContexts = ref(null)
+const currentStep = ref('recording')
+const currentGifSrc = ref('')
+
+const title = computed(() => {
+  return steps[currentStep.value].title
+})
+
+const btnText = computed(() => {
+  return steps[currentStep.value].btnText
+})
+
+const btnBgColorClass = computed(() => {
+  return steps[currentStep.value].btnBgColorClass
+})
+
+const btnTextColorClass = computed(() => {
+  return steps[currentStep.value].btnTextColorClass
+})
 
 const gifshot = inject('gifshot')
+
+const generateGIF = (obj) => {
+  var image = obj.image
+  currentGifSrc.value = image
+}
 
 const startRecording = async () => {
   const videoElement = document.getElementById('videoElement')
 
-  const gifElement = document.getElementById('gifElement')
   // Request access to the camera
   let stream = await navigator.mediaDevices.getUserMedia({ video: true })
   // Assign the stream to the video element's source
@@ -33,9 +70,8 @@ const startRecording = async () => {
 
         if (captureProgress === 1) {
           if (stream) {
-            // Stop all tracks in the stream
+            // Stop the stream
             stream.getTracks().forEach((track) => track.stop())
-            console.log('Stream stopped')
           }
         }
       },
@@ -46,16 +82,14 @@ const startRecording = async () => {
     },
     function (obj) {
       if (!obj.error) {
-        // hide record section
+        // Next step
+        currentStep.value = 'edit'
 
         // Save the rendering contexts for later use
-        savedRenderingContexts.value = obj.savedRenderingContexts
+        currentSavedRenderingContexts.value = obj.savedRenderingContexts
+        savedRenderingContexts.value = [...savedRenderingContexts.value, obj.savedRenderingContexts]
 
-        var image = obj.image,
-          animatedImage = document.createElement('img')
-        animatedImage.src = image
-        animatedImage.id = 'animatedImage'
-        gifElement.appendChild(animatedImage)
+        generateGIF(obj)
       }
     }
   )
@@ -63,17 +97,42 @@ const startRecording = async () => {
 </script>
 
 <template>
-  <TheHeader title="Start by recording a short video" />
+  <TheHeader :title="title" />
 
   <main class="flex flex-col">
-    <VideoItem id="videoElement" height="400" width="400" />
+    <VideoItem id="videoElement" height="400" width="400" v-if="currentStep === 'recording'" />
+    <section id="gifElement" class="mx-auto">
+      <img :src="currentGifSrc" alt="" />
+    </section>
+
+    <section id="addText" class="mx-auto my-3">
+      <h3 class="text-xl">Add Some Text...</h3>
+
+      <input type="text" id="gifText" class="border-2" />
+
+      <h3 class="text-xl">Where?</h3>
+
+      <div class="mb-1">
+        <input type="radio" id="top" name="textPlacement" value="top" />
+        <label for="top" class="ms-2">Top</label>
+      </div>
+
+      <div class="mb-1">
+        <input type="radio" id="middle" name="textPlacement" value="middle" />
+        <label for="middle" class="ms-2">Middle</label>
+      </div>
+
+      <div class="mb-1">
+        <input type="radio" id="bottom" name="textPlacement" value="bottom" checked />
+        <label for="bottom" class="ms-2">Bottom</label>
+      </div>
+    </section>
     <TheButton
       @click="startRecording"
-      text="Start Recording"
-      bgColor="bg-emerald-500"
-      textColor="text-white"
+      :text="btnText"
+      :bgColor="btnBgColorClass"
+      :textColor="btnTextColorClass"
     />
-    <div id="gifElement"></div>
   </main>
 </template>
 
